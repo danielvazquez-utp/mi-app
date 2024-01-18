9691fa312f4aea9a018fb76d39279e0e500b0c9b
Daniel Vazquez - Ubuntu <daniel.vazquez@utpuebla.edu.mx>
2024-01-18 12:10:14 -0600

    Ejercicio contador usando Usestate

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './Miapp.css';
+
+const App = () => {
+
+  const [contador, setContador] = useState(0);
+
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <p>Daniel Vázquez</p>
+      <h2>Contador: { contador }</h2>
+      <button id="btn-incrementa" className="btns" onClick={ () =>  setContador(contador+1) } >Incrementa</button>
+      <button id="btn-decrementa" className="btns">Decrementa</button>
+      <button id="btn-reinicia" className="btns">Reinicia</button>
+    </>
+  )
+}
+
+export default App;
