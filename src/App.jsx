48009778ca222e0bc94613b2154deafa603e4c7a
Daniel Vazquez - Ubuntu <daniel.vazquez@utpuebla.edu.mx>
2024-01-18 12:10:32 -0600

    Falto un elemento

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <h1>Hola mundo</h1>
       <p>Daniel Vázquez</p>
       <h2>Contador: { contador }</h2>
-      <button id="btn-incrementa" className="btns" onClick={ () =>  setContador(contador+1) } >Incrementa</button>
+      <button id="btn-incrementa" className="btns" onClick={() =>  setContador(contador+1) } >Incrementa</button>
       <button id="btn-decrementa" className="btns">Decrementa</button>
       <button id="btn-reinicia" className="btns">Reinicia</button>
     </>
